1202592a4ace22e499fd3ccd8f70f268fde73c54
Daria Bukhtatay <bonum_factum@bk.ru>
2018-12-24 15:24:52 +0600

    Добавлен js код
    Добавлен js код

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var link = document.querySelector(".search");
+var popup = document.querySelector(".search-card");
+
+var popup_class_popup = "search-card-show";
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+
+  if (popup.classList.contains(popup_class_popup)) {
+    popup.classList.remove(popup_class_popup);
+  } else {
+    popup.classList.add(popup_class_popup);
+  }
+});
