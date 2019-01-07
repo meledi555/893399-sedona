52e5277bedbc8e2bb4d352c21cfae013262196fe
Daria Bukhtatay <bonum_factum@bk.ru>
2019-01-07 21:36:27 +0600

    Д20

--- js/script.js
+++ js/script.js
@@ -3,12 +3,14 @@ var popup = document.querySelector(".search-card");
 
 var popup_class_popup = "search-card-show";
 
-link.addEventListener("click", function (evt) {
-  evt.preventDefault();
+if (link){
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
 
-  if (popup.classList.contains(popup_class_popup)) {
-    popup.classList.remove(popup_class_popup);
-  } else {
-    popup.classList.add(popup_class_popup);
-  }
-});
+    if (popup.classList.contains(popup_class_popup)) {
+      popup.classList.remove(popup_class_popup);
+    } else {
+      popup.classList.add(popup_class_popup);
+    }
+  });
+}
